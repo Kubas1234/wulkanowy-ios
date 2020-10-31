04b992b5cbfa1a001488d81d63cdf8e868bc3b74
Mikołaj Pich <m.pich@outlook.com>
2020-10-31 21:22:15 +0100

    Add project files

---
+++ wulkanowy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  wulkanowy
+//
+//  Created by Mikołaj on 25/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Wulkanowy!")
+            .bold()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ wulkanowy/wulkanowyApp.swift
@@ -0,0 +1,17 @@
+//
+//  wulkanowyApp.swift
+//  wulkanowy
+//
+//  Created by Mikołaj on 25/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct wulkanowyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
