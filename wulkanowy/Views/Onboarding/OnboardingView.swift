8761a4366f878d36858f8ec4f7ff2698e0fba3c0
Karol Zientek <contact@karolzientek.tech>
2021-02-13 00:37:02 +0100

    Added Onboarding View with Onboarding Button and needed localizations

--- wulkanowy/Views/Onboarding/OnboardingView.swift
+++ wulkanowy/Views/Onboarding/OnboardingView.swift
@@ -8,13 +8,42 @@
 import SwiftUI
 
 struct OnboardingView: View {
+    
+    @State private var isAnimating: Bool = false
+    
     var body: some View {
-        
+            VStack(spacing: 20) {
+                Spacer()
+                Image("wulkanowy-svg")
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .top)
+                    .foregroundColor(Color("OnboardingColor"))
+                    .scaleEffect(isAnimating ? 1.0 : 0.6)
+                VStack(spacing: 20) {
+                    Text("onboarding.description.title")
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                    Text("onboarding.description.content")
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                }
+                .padding(.horizontal, 20)
+                Spacer()
+                OnboardingButtonView()
+                    .padding()
+            }
+            .onAppear {
+                withAnimation(.easeOut(duration: 0.5)) {
+                    isAnimating = true
+                }
+            }
     }
 }
 
-struct OnboardingView_Previews: PreviewProvider {
+struct WulkanowyCardView_Previews: PreviewProvider {
     static var previews: some View {
-        OnboardingView()
+        Group {
+            OnboardingView().previewLayout(.fixed(width: 320, height: 640))
+        }
     }
 }
