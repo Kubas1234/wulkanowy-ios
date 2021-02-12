2e4d69830645b584e82f58487a8bed568ef2f921
Karol Zientek <contact@karolzientek.tech>
2021-02-12 22:59:52 +0100

    Added app icon and Onboarding view

---
+++ wulkanowy/Views/Onboarding/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  wulkanowy
+//
+//  Created by Karol Zientek on 12/02/2021.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
