f1dcc9016d2cfd7bec90c9e1d9836fbf681f41c9
Pengwius <tomekftomekf@gmail.com>
2021-03-02 11:27:02 +0100

    Fixes on branch

--- wulkanowy/Views/Login/LoginView.swift
+++ wulkanowy/Views/Login/LoginView.swift
@@ -1,48 +1,4 @@
 //
-<<<<<<< HEAD
-//  LoginView.swift
-//  wulkanowy
-//
-//  Created by Karol Zientek on 13/02/2021.
-//
-
-import SwiftUI
-
-struct LoginView: View {
-    
-    @AppStorage("needsAppOnboarding") private var needsAppOnboarding: Bool = true
-    
-    @State private var token: String = ""
-    
-    
-    var body: some View {
-        VStack {
-            Image("wulkanowy-svg")
-                .resizable()
-                .foregroundColor(Color("PrimaryColor"))
-                .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
-            
-            Text("login.login")
-                .font(.largeTitle)
-                .padding(.bottom, 30)
-            
-            CustomTextFieldView(placeholder: "login.token", text: "")
-            
-            CustomTextFieldView(placeholder: "login.symbol", text: "")
-            
-            CustomTextFieldView(placeholder: "login.pin", text: "")
-            
-            Spacer()
-            
-            CustomButtonView(action: {
-                print("Works")
-            }, title: "login.login")
-        }
-        .padding([.top, .horizontal], 20)
-        .sheet(isPresented: $needsAppOnboarding) {
-            OnboardingView()
-        }
-=======
 //  ContentView.swift
 //  wulkanowy
 //
@@ -225,19 +181,14 @@ struct LoginView: View {
         }
         .padding()
         Spacer()
->>>>>>> feature/register-device
     }
 }
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
-<<<<<<< HEAD
-        LoginView()
-=======
         Group {
             LoginView()
         }
         .preferredColorScheme(.dark)
->>>>>>> feature/register-device
     }
 }
