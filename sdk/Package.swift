dbf04b4bd629f0bf12738d17adc659da9dfde1b1
Tomasz F <tomekftomekf@gmail.com>
2021-03-26 16:35:52 +0100

    Update Package.swift

--- sdk/Package.swift
+++ sdk/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Sdk",
-            dependencies: []),
+            dependencies: ["KeychainAccess", "OpenSSL"]),
         .testTarget(
             name: "SdkTests",
             dependencies: ["Sdk"]),
