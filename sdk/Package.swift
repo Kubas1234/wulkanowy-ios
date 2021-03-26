c745d7fb9cbd732feb90d77fc4c4d4fcdd5f14ab
Pengwius <tomekftomekf@gmail.com>
2021-03-26 12:09:05 +0100

    Fix

--- sdk/Package.swift
+++ sdk/Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL", from: "1.1.180"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
